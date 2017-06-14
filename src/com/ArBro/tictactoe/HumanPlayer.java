4c1dca1169284944fcf59d519357d8175f169362
ABrw <arcobrouwer@gmail.com>
2017-06-14 14:24:32 +0200

    Human Player Class added

---
+++ src/com/ArBro/tictactoe/HumanPlayer.java
@@ -0,0 +1,17 @@
+package com.ArBro.tictactoe;
+
+/**
+ * Created by ABrouwer on 14-6-2017.
+ */
+
+public class HumanPlayer extends Player {
+
+    //Constructors
+    //TODO: Let player class determine what the playerID has to be
+    HumanPlayer(int id, Token x) {
+        super(id, x);
+    }
+
+    //Getters & Setters
+
+}
